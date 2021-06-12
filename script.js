ca33fd1479c4f82ddff3b6a4c287b0c1a87e494f
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-12 18:26:35 -0300

    Select search element

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const api = {
     key:"362438f27fa7719c58ad9e86c29192fa",
     url:"https://api.openweathermap.org/data/2.5/",
-}
+} // adicionar chave e URL de API
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
